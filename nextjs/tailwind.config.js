ef3ebf1c525c85c0c83a93cdb7bff50de6413dcb
Chris Olsen <chrisevjenolsen@gmail.com>
2025-01-10 10:05:40 -0500

    include the tailwind and postcss config files

---
+++ nextjs/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,jsx}", // Includes all pages
+    "./src/components/**/*.{js,jsx}", // Includes all components
+  ],
+  theme: {
+    extend: {}, // Customize your theme here
+  },
+  plugins: [],
+};
